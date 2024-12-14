0bc3fa6f274168c6b5cad561dc5b8f3035a16093
microcai <microcaicai@gmail.com>
2024-12-14 23:17:35 +0800

    添加一个 C 版本的，使用 ucontext/ Windows Fiber 的协程示例

---
+++ example/echo_server/echo_server.c
@@ -0,0 +1,265 @@
+﻿// iocp_echo_server.c
+// -----------------------------------------------------------------------------
+
+#include "universal_fiber.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// -----------------------------------------------------------------------------
+
+enum // configuration
+{
+   MAX_BUF = 1024,
+   SERVER_ADDRESS = INADDR_ANY,
+   SERVICE_PORT = 50001
+};
+
+// -----------------------------------------------------------------------------
+
+// the completion port
+static HANDLE cpl_port;
+
+// the listening socket
+static SOCKET listener;
+
+// -----------------------------------------------------------------------------
+
+// prototypes - main functions
+static void init(void);
+// prototypes - helper functions
+
+static void bind_listening_socket(void);
+static SOCKET create_accepting_socket(void);
+static void create_io_completion_port(void);
+static void create_listening_socket(void);
+static BOOL get_completion_status(DWORD*, ULONG_PTR*, OVERLAPPED**);
+static void init_winsock(void);
+static void prepare_endpoint(struct sockaddr_in*, u_long, u_short);
+static void start_listening(void);
+
+// -----------------------------------------------------------------------------
+
+int main(void)
+{
+   init();
+   run_event_loop(cpl_port);
+   return 0;
+}
+
+// -----------------------------------------------------------------------------
+
+static void bind_listening_socket(void)
+{
+   struct sockaddr_in sin;
+
+   prepare_endpoint(&sin, SERVER_ADDRESS, SERVICE_PORT);
+   int v = 1;
+   setsockopt(SOCKET_get_fd(listener), SOL_SOCKET, SO_REUSEADDR, (char*)&v, sizeof v);
+   if (bind(SOCKET_get_fd(listener), (struct sockaddr*) &sin, sizeof(sin)) == SOCKET_ERROR)
+   {
+      printf("* error in bind!\n");
+      exit(1);
+   }
+}
+
+// -----------------------------------------------------------------------------
+
+static SOCKET create_accepting_socket(void)
+{
+   SOCKET acceptor = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, WSA_FLAG_OVERLAPPED);
+   if (acceptor == INVALID_SOCKET)
+   {
+      printf("* error creating accept socket!\n");
+      exit(1);
+   }
+   return acceptor;
+}
+
+// -----------------------------------------------------------------------------
+
+static void create_io_completion_port(void)
+{
+   cpl_port = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+   if (!cpl_port)
+   {
+      int err = WSAGetLastError();
+      printf("* error %d in line %d CreateIoCompletionPort\n", err, __LINE__);
+      exit(1);
+   }
+}
+
+// -----------------------------------------------------------------------------
+
+static void create_listening_socket(void)
+{
+   listener = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0 , 0, WSA_FLAG_OVERLAPPED);
+   if (listener == INVALID_SOCKET)
+   {
+      printf("* error creating listening socket!\n");
+      exit(1);
+   }
+
+   if (CreateIoCompletionPort((HANDLE)listener, cpl_port, 0, 0) != cpl_port)
+   {
+      int err = WSAGetLastError();
+      printf("* error %d in listener\n", err);
+      exit(1);
+   }
+}
+
+static void client_coroutine(void* param)
+{
+   SOCKET client_sock = (SOCKET) param;
+
+   char buf1[1024];
+
+   WSABUF wsabuf1 = {
+      .len = 1024,
+      .buf = buf1
+   };
+
+   FiberOVERLAPPED ov;
+   memset(&ov, 0, sizeof(ov));
+
+   DWORD recv_bytes = 0, flag = 0;
+
+   if (WSARecv(client_sock, &wsabuf1, 1, &recv_bytes, &flag, &ov.ov, NULL) == SOCKET_ERROR)
+   {
+      int err = WSAGetLastError();
+      if (err != WSA_IO_PENDING)
+      {
+         printf("*error %d in WSARecv\n", err);
+         closesocket(client_sock);
+         return;
+      }
+   }
+
+   recv_bytes = get_overlapped_result(&ov);
+   printf("* read operation completed\n");
+
+   char buf2[1024];
+   int content_length_line_len = snprintf(buf2, 80, "Content-Length: %d\r\n\r\n", ov.byte_transfered);
+
+   WSABUF wsabuf[3] = {
+      { .len = 17, .buf = (char*) "HTTP/1.1 200 OK\r\n" },
+      { .len = (size_t) content_length_line_len, .buf = buf2 },
+      { ov.byte_transfered, buf1 }
+   };
+
+   memset(&ov, 0, sizeof(ov));
+
+   if (WSASend(client_sock, wsabuf, 3, NULL, 0, &ov.ov, NULL) == SOCKET_ERROR)
+   {
+      int err = WSAGetLastError();
+      if (err != WSA_IO_PENDING)
+      {
+         printf("*error %d in WSASend\n", err);
+         closesocket(client_sock);
+         return;
+      }
+   }
+
+   get_overlapped_result(&ov);
+   printf("* write operation completed\n");
+
+   closesocket(client_sock);
+   printf("client_coroutine exit\n");
+   return;
+}
+
+static void accept_coroutine(void* param)
+{
+   for (;;)
+   {
+      SOCKET new_client_socket = create_accepting_socket();
+      DWORD expected = sizeof(struct sockaddr_in) + 16;
+
+      printf("* started accepting connections...\n");
+
+      FiberOVERLAPPED listener_ovl;
+      memset(&listener_ovl, 0, sizeof(listener_ovl));
+      // uses listener's completion key and overlapped structure
+      char buf[1024];
+
+      // starts asynchronous accept
+      if (!AcceptEx(listener, new_client_socket, buf, 0 /* no recv */,
+         expected, expected, NULL, &listener_ovl.ov))
+      {
+         int err = WSAGetLastError();
+         if (err != ERROR_IO_PENDING)
+         {
+            printf("* error %d in AcceptEx\n", err);
+            exit(1);
+         }
+      }
+
+      get_overlapped_result(&listener_ovl);
+
+      if (listener_ovl.resultOk)
+      {
+         CreateIoCompletionPort((HANDLE)new_client_socket, cpl_port, 0, 0);
+
+         create_detached_coroutine(client_coroutine, (VOID*)new_client_socket,"client_coro");
+      }
+      else
+      {
+         closesocket(new_client_socket);
+      }
+   }
+
+   return ;
+}
+
+static void init(void)
+{
+   init_winsock();
+   create_io_completion_port();
+   create_listening_socket();
+   bind_listening_socket();
+   start_listening();
+
+   ConvertThreadToFiber(0);
+
+   // 并发投递 64 个 accept 操作。加快 accept 速度.
+   for (int i=0; i < 64; i++)
+      create_detached_coroutine(accept_coroutine, 0, "accept_coro");
+
+   printf("back\n");
+}
+
+// -----------------------------------------------------------------------------
+
+static void init_winsock(void)
+{
+   WSADATA wsaData;
+
+   if (WSAStartup(MAKEWORD(2,2), &wsaData))
+   {
+      printf("* error in WSAStartup!\n");
+      exit(1);
+   }
+}
+
+// -----------------------------------------------------------------------------
+
+static void prepare_endpoint(struct sockaddr_in* sin, u_long address,
+   u_short port)
+{
+   sin->sin_family = AF_INET;
+   sin->sin_addr.s_addr = htonl(address);
+   sin->sin_port = htons(port);
+}
+
+static void start_listening(void)
+{
+   if (listen(SOCKET_get_fd(listener), 100) == SOCKET_ERROR)
+   {
+      printf("* error in listen!\n");
+      exit(1);
+   }
+   printf("* started listening for connection requests...\n");
+}
+// -----------------------------------------------------------------------------
+// the end
---
+++ µasync/include/universal_fiber.h
@@ -0,0 +1,199 @@
+﻿
+#ifndef ___UNIVERSAL_FIBER__H___
+#define ___UNIVERSAL_FIBER__H___
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <mswsock.h>
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
+#ifndef SOCKET_get_fd
+#define SOCKET_get_fd(x) (x)
+#endif
+#else
+#include "iocp.h"
+
+#include <ucontext.h>
+
+#endif
+
+typedef struct
+{
+	OVERLAPPED ov;
+#ifdef _WIN32
+	LPVOID target_fiber;
+#else
+	ucontext_t target;
+#endif
+	DWORD byte_transfered;
+	DWORD last_error;
+	ULONG_PTR completekey;
+	BOOL resultOk;
+} FiberOVERLAPPED;
+
+#ifdef _WIN32
+extern __declspec(thread) LPVOID __current_yield_fiber = NULL;
+extern __declspec(thread) LPVOID __please_delete_me = NULL;
+
+#else
+extern _Thread_local ucontext_t* __current_yield_ctx = NULL;
+#endif
+
+// wait for overlapped to became complete. return NumberOfBytes
+inline DWORD get_overlapped_result(FiberOVERLAPPED* ov)
+{
+#ifdef _WIN32
+	ov->target_fiber = GetCurrentFiber();
+	SwitchToFiber(__current_yield_fiber);
+#else
+	swapcontext(&ov->target, __current_yield_ctx);
+#endif
+	DWORD R = ov->byte_transfered;
+	WSASetLastError(ov->last_error);
+	return R;
+}
+
+// call this after GetQueuedCompletionStatus.
+inline void process_overlapped_event(OVERLAPPED* _ov, BOOL resultOk, DWORD NumberOfBytes, ULONG_PTR complete_key,
+									 DWORD last_error)
+{
+	FiberOVERLAPPED* ovl_res = (FiberOVERLAPPED*)(_ov);
+	ovl_res->byte_transfered = NumberOfBytes;
+	ovl_res->last_error = last_error;
+	ovl_res->completekey = complete_key;
+	ovl_res->resultOk = resultOk;
+
+#ifdef _WIN32
+
+	LPVOID old = __current_yield_fiber;
+	__current_yield_fiber = GetCurrentFiber();
+	SwitchToFiber(ovl_res->target_fiber);
+	__current_yield_fiber = old;
+	if (__please_delete_me)
+	{
+		DeleteFiber(__please_delete_me);
+		__please_delete_me = NULL;
+	}	
+#else
+	ucontext_t self;
+	ucontext_t* old = __current_yield_ctx;
+	__current_yield_ctx = &self;
+	swapcontext(&self, &ovl_res->target);
+	__current_yield_ctx = old;
+#endif
+}
+
+inline void run_event_loop(HANDLE iocp_handle)
+{
+	for (;;)
+	{
+		DWORD NumberOfBytes = 0;
+		ULONG_PTR ipCompletionKey = 0;
+		LPOVERLAPPED ipOverlap = NULL;
+
+		// get IO status, no wait
+		SetLastError(0);
+		BOOL ok = GetQueuedCompletionStatus(iocp_handle, &NumberOfBytes, (PULONG_PTR)&ipCompletionKey, &ipOverlap, INFINITE);
+		DWORD last_error = GetLastError();
+
+		if (ipOverlap)
+		{
+			process_overlapped_event(ipOverlap, ok, NumberOfBytes, ipCompletionKey, last_error);
+		}
+	}
+}
+
+#ifdef _WIN32
+
+typedef  struct{
+   LPVOID caller_fiber;
+   void (*func_ptr)(void* param);
+   void* param;
+} FiberParamPack;
+
+static inline void WINAPI FiberEntryPoint(LPVOID param)
+{
+   FiberParamPack saved_param;
+   memcpy(&saved_param, param, sizeof(FiberParamPack));
+   SwitchToFiber(saved_param.caller_fiber);
+
+   saved_param.func_ptr(saved_param.param);
+   
+   __please_delete_me = GetCurrentFiber();
+   SwitchToFiber(__current_yield_fiber);
+}
+
+#else
+static inline void __coroutine_entry_point(void (*func_ptr)(void* param), void* param, ucontext_t* self_ctx,
+										   void* stack_pointer)
+{
+	func_ptr(param);
+
+	// 然后想办法 free(stack_pointer);
+
+	ucontext_t helper_ctx;
+	_Thread_local static char helper_stack[256];
+
+	getcontext(&helper_ctx);
+	typedef void (*__func)(void);
+
+	helper_ctx.uc_stack.ss_sp = helper_stack;
+	helper_ctx.uc_stack.ss_flags = 0;
+	helper_ctx.uc_stack.ss_size = 256;
+	helper_ctx.uc_link = __current_yield_ctx; // self_ctx->uc_link;
+
+	free(self_ctx);
+
+	makecontext(&helper_ctx, (__func)free, 1, stack_pointer);
+	setcontext(&helper_ctx);
+}
+#endif // _WIN32
+
+inline void create_detached_coroutine(void (*func_ptr)(void* param), void* param, char* coro_name)
+{
+#ifdef _WIN32
+
+	FiberParamPack fiber_param;
+	fiber_param.func_ptr = func_ptr;
+	fiber_param.param = param;
+	fiber_param.caller_fiber = GetCurrentFiber();
+
+	LPVOID new_fiber = CreateFiber(0, FiberEntryPoint, &fiber_param);
+
+	SwitchToFiber(new_fiber);
+	LPVOID old = __current_yield_fiber;
+	__current_yield_fiber = GetCurrentFiber();
+	SwitchToFiber(new_fiber);
+	__current_yield_fiber = old;
+	if (__please_delete_me)
+	{
+		DeleteFiber(__please_delete_me);
+		__please_delete_me = NULL;
+	}
+
+#else  // _WIN32
+	ucontext_t* new_ctx = (ucontext_t*)calloc(1, sizeof(ucontext_t));
+	void* new_stack = malloc(8192);
+
+	typedef void (*__func)(void);
+
+	ucontext_t self;
+	ucontext_t* old = __current_yield_ctx;
+	__current_yield_ctx = &self;
+
+	getcontext(new_ctx);
+	new_ctx->uc_stack.ss_sp = new_stack;
+	new_ctx->uc_stack.ss_flags = 0;
+	new_ctx->uc_stack.ss_size = 8000;
+	new_ctx->uc_link = __current_yield_ctx;
+	makecontext(new_ctx, (__func)__coroutine_entry_point, 4, func_ptr, param, new_ctx, new_stack);
+
+	swapcontext(&self, new_ctx);
+	__current_yield_ctx = old;
+#endif // _WIN32
+}
+
+#endif // ___UNIVERSAL_FIBER__H___
