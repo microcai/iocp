a9272c2da8e0a05082e436e32e8fa3c0b8410681
microcai <microcaicai@gmail.com>
2024-12-26 09:12:35 +0800

    zcontext improve to support clang-cl @ windows

--- uasync/include/zcontext.h
+++ uasync/include/zcontext.h
@@ -1,11 +1,51 @@
 
 #pragma once
 
+#include <cstdint>
+#include <float.h>
+
 struct zcontext_t
 {
     void* sp;// pointer to active stack buttom
 };
 
-void* zcontext_swap(zcontext_t* from, zcontext_t* to, void* argument) asm("zcontext_swap");
+extern void* zcontext_swap(zcontext_t* from, zcontext_t* to, void* argument) asm("zcontext_swap");
+extern void* zcontext_entry_point() asm("zcontext_entry_point");
 
-void zcontext_setup(zcontext_t* target, void (*func)(void*arg), void* argument) asm("zcontext_setup");
+inline void zcontext_setup(zcontext_t* target, void (*func)(void*arg), void* argument)
+{
+    struct startup_stack_structure
+    {
+#ifdef _WIN32
+        void* fc_x87_cw;
+        void* fc_mxcsr;
+        void* mmx_reg[20];
+        void* pad;
+        void* general_reg[8];
+#else
+        void* fc_x87_cw;
+        void* fc_mxcsr;
+        void* general_reg[6];
+#endif
+        void* ret_address;
+        void* param1;
+        void* param2;
+
+#ifdef _WIN32
+        uint32_t* padding[4];
+#endif
+    };
+
+    char* sp = reinterpret_cast<char*>(target->sp);
+
+    sp -= sizeof(startup_stack_structure);
+
+    target->sp = sp;
+
+    auto startup_stack = reinterpret_cast<startup_stack_structure*>(sp);
+
+    startup_stack->ret_address = (void*) zcontext_entry_point;
+    startup_stack->param1 = (void*) func;
+    startup_stack->param2 = argument;
+    startup_stack->fc_mxcsr = startup_stack->fc_x87_cw = 0;
+}
